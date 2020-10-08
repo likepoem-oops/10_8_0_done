c55fb05bf1f8185a58f393bf1b4643ece260ea36
likepoem-oops <2605597055@qq.com>
2020-10-08 10:02:14 +0800

    数组学习2

--- main.c
+++ main.c
@@ -44,7 +44,22 @@ int main()
     for (int i = 6; i >= 0 ; --i) {
         people[i] = people[i] - 1;
         printf("%d ",people[i]);
-
     }
+    printf("\n");
+//更改数组顺序
+    for (int i = 0; i < (6-i) ; ++i) {
+        //交换i 和6-i的值
+        //使用临时变量保存一个数
+        int temp = people[i];
+        //将一个数写到另一个变量中
+        people[i] = people[6 - i];
+        //将临时变量的值赋予第二个变量中
+        people[6-i] = temp;
+    }
+    for (int i = 0; i < 7 ; ++i) {
+        printf("%d ",people[i]);
+    }
+
+
     return 0;
 }
